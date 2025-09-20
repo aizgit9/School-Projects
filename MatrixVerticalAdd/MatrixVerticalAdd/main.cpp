04f98d0da115e54302d157dd7ba6f6a9cb34d974
asherisgitt <asher.isgitt@gmail.com>
2025-09-20 15:06:19 -0500

    add final logic tweaks and comments

--- MatrixVerticalAdd/MatrixVerticalAdd/main.cpp
+++ MatrixVerticalAdd/MatrixVerticalAdd/main.cpp
@@ -1,5 +1,3 @@
-// Put in a prologue
-
 /*
 * Program Name: MatrixVerticalAdd
 * Date:         2025-9-19
@@ -28,72 +26,73 @@ void displayCalcMatrixSums (int [][MAX_MATRIX_SIZE], unsigned,  unsigned,  int[M
 void   symmetryCheckMatrix (int [], unsigned,  unsigned);
 void        sortMatrixRows (int [][MAX_MATRIX_SIZE], unsigned,  unsigned);
 
-
 int main() {
-  unsigned height,
-           width;
-  int      matrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE],
-           colSums[MAX_MATRIX_SIZE];
-  string   INPUT_FILE_NAME = "matrixes.txt";
-  ifstream inputFileStreamObj(INPUT_FILE_NAME);
+    unsigned height,
+            width;
+    int      matrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE],
+            colSums[MAX_MATRIX_SIZE];
+    string   INPUT_FILE_NAME = "matrixes.txt";
+    ifstream inputFileStreamObj(INPUT_FILE_NAME);
 
-  if (inputFileStreamObj.fail()) {
-     cout << "File " << INPUT_FILE_NAME << "could not be opened !" << endl; 
-     cout << endl << "Press the enter key once or twice to leave..." << endl; cin.ignore(); cin.get();
-     exit(EXIT_FAILURE); 
-   }
+    // Display file reading error message
+    if (inputFileStreamObj.fail()) {
+        cout << "File " << INPUT_FILE_NAME << "could not be opened !" << endl; 
+        cout << endl << "Press the enter key once or twice to leave..." << endl; cin.ignore(); cin.get();
+        exit(EXIT_FAILURE); 
+    }
 
-   do {  
+    do {
+        //Ends program when the reader reaches the end of the file
+        if (readMatrixFromFile(matrix, height, width, inputFileStreamObj) )
+        break;
 
-     if (readMatrixFromFile(matrix, height, width, inputFileStreamObj) )
-       break;
+        //Displays the input matrix
+        cout << "Input:" << endl;
+        displayMatrix        (matrix, height, width);
 
-     cout << "Input:" << endl;
-     displayMatrix        (matrix, height, width);
-     displayCalcMatrixSums(matrix, height, width, colSums);
-     symmetryCheckMatrix  (colSums, width, height);
-     sortMatrixRows       (matrix,  width, height);
-     cout << "Sorted:" << endl;
-     displayMatrix        (matrix, height, width);
+        //Displays the column sums and symmetry check result
+        displayCalcMatrixSums(matrix, height, width, colSums);
+        symmetryCheckMatrix  (colSums, width, height);
 
-     cout << endl << "Press the enter key once or twice to continue..." << endl; cin.ignore(); cin.get(); 
+        //Displays the sorted matrix
+        sortMatrixRows(matrix, width, height);
+        cout << "Sorted:" << endl;
+        displayMatrix        (matrix, height, width);
+
+        cout << endl << "Press the enter key once or twice to continue..." << endl; cin.ignore(); cin.get(); 
      
-   } while (true);
+    } while (true);
 
-   cout << "Program Done" << endl;
+    cout << "Program Done" << endl;
 
-   exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
-/*
-* Reads the height and width of the matrix from the input file.
-* Loops through the input file numbers and adds them to the matrix array.
-*/
-bool readMatrixFromFile(int matrix[][MAX_MATRIX_SIZE], unsigned& height, unsigned& width, ifstream& inputFileStreamObj) 
-{
+// Read the height and width of the matrix from the input file
+// Loop through the input file numbers and add them to the matrix array
+bool readMatrixFromFile(int matrix[][MAX_MATRIX_SIZE], unsigned& height, unsigned& width, ifstream& inputFileStreamObj) {
     inputFileStreamObj >> height;
     inputFileStreamObj >> width;
 
-    for (int row = 0; row < height; row++)
-    {
-        for (int col = 0; col < width; col++)
-        {
-             inputFileStreamObj >> matrix[row][col];
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+
+            // Returns true when the end of the file is reached
+            if (!(inputFileStreamObj >> matrix[row][col])) {
+                return true;
+            }
         }
     }
 
     return(false);
 }
 
-// Display the values of the matrix in a rectangular tabular format.
-void displayMatrix(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width) 
-{
-    for (int row = 0; row < height; row++)
-    {
+// Display the values of the matrix in a rectangular tabular format
+void displayMatrix(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width) {
+    for (int row = 0; row < height; row++) {
         cout << "\t";
 
-        for (int col = 0; col < width; col++)
-        {
+        for (int col = 0; col < width; col++) {
             cout << std::right << std::setw(5) << matrix[row][col] << "\t";
         }
 
@@ -103,21 +102,16 @@ void displayMatrix(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned widt
     cout << endl;
 }
 
-/*
-* Adds up the sums of each column in the matrix.
-* Display the sums in a rectangular tabular format.
-*/
-void displayCalcMatrixSums(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width, int colSums[MAX_MATRIX_SIZE]) 
-{
+// Add up the sums of each column in the matrix
+// Display the sums in a rectangular tabular format
+void displayCalcMatrixSums(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width, int colSums[MAX_MATRIX_SIZE]) {
     cout << "Sums:" << endl;
     cout << "\t";
 
-    for (int col = 0; col < width; col++)
-    {
+    for (int col = 0; col < width; col++) {
         int colSum = 0;
 
-        for (int row = 0; row < height; row++)
-        {
+        for (int row = 0; row < height; row++) {
             colSum += matrix[row][col];
         }
         
@@ -130,26 +124,22 @@ void displayCalcMatrixSums(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsig
  }
  
 
-// Checks colSums to see if the array has vertical additive symmetry.
-void symmetryCheckMatrix(int colSums[], unsigned width, unsigned height) 
-{
+// Check colSums to see if the array has vertical additive symmetry
+void symmetryCheckMatrix(int colSums[], unsigned width, unsigned height) {
     cout << "Vertical additive symmetry: ";
 
     int reversedColSums[MAX_MATRIX_SIZE] = { 0 };
     int j = 0;
 
-    // Reversing colSums
-    for (int i = width - 1; i >= 0; i--)
-    {
+    // Reverse colSums
+    for (int i = width - 1; i >= 0; i--) {
         reversedColSums[j] = colSums[i];
         j++;
     }
 
-    // Comparing reversedColSums with colSums
-    for (int i = 0; i < width; i++) 
-    {
-        if ((reversedColSums[i] != colSums[i]))
-        {
+    // Compare reversedColSums with colSums
+    for (int i = 0; i < width; i++) {
+        if ((reversedColSums[i] != colSums[i])) {
             cout << "No" << endl << endl;
 
             return;
@@ -159,11 +149,9 @@ void symmetryCheckMatrix(int colSums[], unsigned width, unsigned height)
     cout << "Yes" << endl << endl;
 }
 
-// Sorts each row in the array in ascending order
-void sortMatrixRows(int matrix[][MAX_MATRIX_SIZE], unsigned width, unsigned height) 
-{
-    for (int row = 0; row < height; row++)
-    {
+// Sort each row in the array in ascending order
+void sortMatrixRows(int matrix[][MAX_MATRIX_SIZE], unsigned width, unsigned height) {
+    for (int row = 0; row < height; row++) {
         std::sort(matrix[row], matrix[row] + width);
     }
  }
