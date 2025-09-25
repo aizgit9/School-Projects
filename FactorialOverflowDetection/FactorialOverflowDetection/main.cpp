143181e777e37da4c9953807fd4a7a1cd55e0f34
asherisgitt <asher.isgitt@gmail.com>
2025-09-25 16:28:16 -0500

    add main method functionality

--- FactorialOverflowDetection/FactorialOverflowDetection/main.cpp
+++ FactorialOverflowDetection/FactorialOverflowDetection/main.cpp
@@ -17,6 +17,7 @@ bool isSafeMultiply(TypeMultiply mult01, TypeMultiply mult02, TypeMultiply& mult
     if so, set multResult to 0 and leave with a true
     */
 
+    
     /*$$
     calculate the multResult from mult01 and mult02
     check if mult01 is equal to the multResult divided by mult02
@@ -82,8 +83,8 @@ private:
 public:
     // Constructor
     FactorialCalculationClass(unsigned typeSize) {
-        //$$ set typeSizeBits to typeSize;
-    }
+        typeSizeBits = typeSize;
+     }
 
     void executeFactorialForType() {
 
@@ -107,21 +108,32 @@ public:
 
 int main() {
     // condition cout set to local digit separation comas (USA)
-
+    cout.imbue(locale(""));
 
     /*$$
     create an object of FactorialCalculationClass <uintnn_t> called factorialCalculateUintnnObj(nn)
-    where nn is the size in bits for the tempalte type uintnn_t, the object name factorialCalculateUintnnObj
+    where nn is the size in bits for the template type uintnn_t, the object name factorialCalculateUintnnObj
     and the nn for the argument (nn) to the constructor
     nn must equal nn = 16, 32 and 64 to make the 3 objects
     */
+
+    FactorialCalculationClass<uint16_t> factorialCalculateUint16Obj(16);
+    FactorialCalculationClass<uint32_t> factorialCalculateUint32Obj(32);
+    FactorialCalculationClass<uint64_t> factorialCalculateUint64Obj(64);
 
     /*$$
     for each of the 3 nn objects factorialCalculateUintnnObj
     invoke the executeFactorialForType()
     */
 
+    factorialCalculateUint16Obj.executeFactorialForType();
+    factorialCalculateUint32Obj.executeFactorialForType();
+    factorialCalculateUint64Obj.executeFactorialForType();
+
     // Hold the screen
+    cout << "Press enter key once or twice to end ... ";
+    cin.ignore(); 
+    cin.get();
 
     exit(EXIT_SUCCESS);
 }
