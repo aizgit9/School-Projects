c0a272fa342872aecf2ddd63a06ed812655d864c
asherisgitt <asher.isgitt@gmail.com>
2026-02-18 17:02:33 -0600

    bring all CS 2337 projects up to date

---
+++ ShuntingYardPostFixBET/ShuntingYardPostFixBET/main.cpp
@@ -0,0 +1,357 @@
+// ShuntingYardTemplateSol.cpp : Defines the entry point for the console application.
+/*
+* Program Name: ShuntingYardPostFixBET
+* Date:         2025-12-8
+* Author:       Asher Isgitt
+* Module Purpose
+* This program takes infix expressions from an input file and:
+* uses the Shunting-Yard algorithm to convert them into postfix notation
+* builds a Binary Expression Tree (BET) from the postfix expression
+* displays the expressions with three different traversal methods
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+string inputFileNameStr = "expressions.txt";                  // Default location in solution folder
+
+class OperatorMapClass {
+
+private:
+  typedef map<char, int>    OperatorPrecedenceMapType;
+  OperatorPrecedenceMapType operatorMapObj;
+
+public:
+
+  OperatorMapClass() {
+    operatorMapObj.insert(OperatorPrecedenceMapType::value_type('+', 1));
+    operatorMapObj.insert(OperatorPrecedenceMapType::value_type('-', 1));
+    // xxx insert code here to insert * and / in the map object
+    operatorMapObj.insert(OperatorPrecedenceMapType::value_type('*', 2));
+    operatorMapObj.insert(OperatorPrecedenceMapType::value_type('/', 2));
+  }//OperatorMapClass ()
+
+  bool isStackOperatorHigherPrecedence(char operatorChar, char operatorStackChar) {
+    return((operatorMapObj.count(operatorStackChar))
+      &&
+      (operatorMapObj.at(operatorStackChar) >= operatorMapObj.at(operatorChar)));
+  }//isStackOperatorHigherPrecedence()
+
+  bool  isOperator(char token) {
+      // xxx check if token operator Map Object is 0 or not to return true or false
+      if (operatorMapObj.count(token)) {
+          return true;
+      }
+
+      return false; // dummy return
+   
+  }//isOperator()
+
+};//OperatorMapClass
+
+OperatorMapClass  operatorMapObj;
+
+class ShuntingYardClass {
+
+public:
+
+  string createPostFixFrom(string infixString) {
+
+    string       outputString;
+    stack <char> operatorStackObj;
+
+    for (char token : infixString) {
+      switch (token) {
+      case '/': case '*': case '+': case '-':
+        // xxx insert code here
+        /*
+
+         while (the stack is not empty
+                  AND
+                the top of the stack token is not a left parenthesis
+                  AND
+                precedence of the current operator <= precedence of the top of the stack token
+               )
+         {
+              Push back the stack top token to the output string
+              Pop the stack top and discard it
+          }//while-end
+
+           Push the current operator token onto the stack
+        */
+
+        while (!operatorStackObj.empty() 
+            && operatorStackObj.top() != '(' 
+            && operatorMapObj.isStackOperatorHigherPrecedence(token, operatorStackObj.top())) {
+
+            outputString.push_back(operatorStackObj.top());
+            operatorStackObj.pop();
+        }
+
+        operatorStackObj.push(token);
+
+        break;
+
+
+      case '(':                                                       // left parenthesis                   
+          // xxx insert code here
+          // push this token on the stack
+          operatorStackObj.push('(');
+        break;
+      case ')':
+        // xxx insert code here                                                      // right parenthesis
+        /*
+
+        while (the stack is not empty AND the top stack token is not a left parenthesis)
+        {
+           Push back the stack top token to the output string
+        }//while-end
+
+        Pop the left parenthesis from the stack and discard it
+        */
+          while (!operatorStackObj.empty()
+              && operatorStackObj.top() != '(') {
+              outputString.push_back(operatorStackObj.top());
+              operatorStackObj.pop();
+          }
+
+          operatorStackObj.pop();
+        break;
+      default:                                                        // operand                                                         
+          // xxx insert code here
+          // push back the operand symbol to the output string 
+          outputString.push_back(token);
+        break;
+      }//switch
+    }//for
+
+    // xxx insert code here
+    /*
+     while (the stack is not empty)
+     {
+       //Push back any remaining stack tokens to the output string
+    }//while-end
+    */
+
+    while (!operatorStackObj.empty()) {
+        outputString.push_back(operatorStackObj.top());
+        operatorStackObj.pop();
+    }
+
+
+    return(outputString);
+
+  }//postfix()	
+
+};//ShuntingYardClass
+
+
+
+class TreeNodeClass {
+
+public:
+    TreeNodeClass* left;
+    char            value;
+    TreeNodeClass* right;
+
+};//TreeNodeClass
+
+TreeNodeClass* BuildNewNodeObjPtrMethod(char value) {
+
+    // xxx new a new TreeNodeClass pointer
+    // set value in new node and left and right pointers
+    // return new node pointer
+
+    TreeNodeClass* newNodePtr = new TreeNodeClass;
+
+    newNodePtr->value = value;
+    newNodePtr->left = nullptr;
+    newNodePtr->right = nullptr;
+
+    return newNodePtr; // dummy return
+};
+
+
+TreeNodeClass* ConstructBET(string postFixStr) {
+    stack<TreeNodeClass*>   parseStack;
+    TreeNodeClass*          newNodePtr;
+    OperatorMapClass        OperatorMapObj;
+
+    // xxx must develop code here
+    // Process each character of the post-fix expression into token
+    for(char token : postFixStr)
+    {
+        // Form a new node pointer
+        newNodePtr = BuildNewNodeObjPtrMethod(token);
+
+        // check if an operator
+        if(OperatorMapObj.isOperator(token))
+        {
+            // parse stack nodes into a new subtree as children
+
+            if (parseStack.size() >= 2) {
+                newNodePtr->right = parseStack.top();
+                parseStack.pop();
+                newNodePtr->left = parseStack.top();
+                parseStack.pop();
+            }
+            // Save/Add this sub tree node to the stack
+            parseStack.push(newNodePtr);
+
+        }
+        // not operator
+        else
+        {
+            // operand, push node onto stack
+            parseStack.push(BuildNewNodeObjPtrMethod(token));
+        }
+    }
+
+    //  Place formed root node on the stack into tree
+    newNodePtr = parseStack.top();
+    
+    return newNodePtr;
+}
+
+string buildString;
+
+void preorder(TreeNodeClass* treeNode) {
+    //xxx do pre order transversal to build string
+    if (treeNode) {
+        buildString += treeNode->value;
+        preorder(treeNode->left);
+        preorder(treeNode->right);
+    } 
+}
+
+bool areParensRequired(TreeNodeClass* child, char parentOperator, bool isRightChild) {
+
+    OperatorMapClass operatorMapObj;
+
+    if (!child) {
+        return false;
+    }
+
+    if (!operatorMapObj.isOperator(parentOperator) || !operatorMapObj.isOperator(child->value)) {
+        return false;
+    }
+
+    bool childGEParent = operatorMapObj.isStackOperatorHigherPrecedence(parentOperator, child->value);
+    if (!childGEParent) {
+        // if child precedence is less than parent precedence, parentheses required
+        return true;
+    }
+
+    bool parentGEChild = operatorMapObj.isStackOperatorHigherPrecedence(child->value, parentOperator);
+    if (childGEParent && parentGEChild) {
+        // equal precedence means right child of '-' or '/' needs parentheses
+        if (isRightChild && (parentOperator == '-' || parentOperator == '/')) return true;
+        return false;
+    }
+
+    // child precedence greater than parent precedence, no parentheses
+    return false;
+}
+
+void inorder(TreeNodeClass* treeNode) {
+    //xxx do in order transversal to build string
+    bool parensRequired = false;
+    if (treeNode != nullptr) {
+        // xxx check if parens required pass arguments treeNode->left, treeNode->value
+        // go left
+        if (areParensRequired(treeNode->left, treeNode->value, false        )) {
+            buildString += '(';
+            inorder(treeNode->left);
+            buildString += ')';
+        } else {
+            inorder(treeNode->left);
+        }
+
+        // add value to build string 
+
+        buildString += treeNode->value;
+
+        // xxx check if parens required pass arguments treeNode->right, treeNode->value
+        // go right
+        if (areParensRequired(treeNode->right, treeNode->value, true)) {
+            buildString += '(';
+            inorder(treeNode->right);
+            buildString += ')';
+        } else {
+            inorder(treeNode->right);
+        }
+
+         // check if parens required 
+            //add ) to buildString
+
+        if (parensRequired) {
+            buildString += ')';
+        }
+     
+    }//if
+}
+
+void postorder(TreeNodeClass* treeNode) {
+    //xxx do post order transversal to build string
+        if (treeNode) {
+            postorder(treeNode->left);
+
+            postorder(treeNode->right);
+            
+            buildString += treeNode->value;
+        }
+}
+
+
+
+int main() {
+
+  ifstream  inputFileStreamObj(inputFileNameStr);
+
+  if (inputFileStreamObj.fail()) {
+    cout << "File could not be opened !" << endl;
+
+    return (EXIT_FAILURE);
+  }//if
+
+  string  infixExpressionStr,
+          postfixExpressionStr;
+
+  ShuntingYardClass shuntingYardObj;
+
+  while (inputFileStreamObj >> infixExpressionStr) {
+
+      cout << "InFix   Expression : " << infixExpressionStr << endl;
+      postfixExpressionStr = shuntingYardObj.createPostFixFrom(infixExpressionStr);
+      cout << "PostFix Expression : " << postfixExpressionStr << endl << endl;
+
+      TreeNodeClass* expressionTreeRootPtr = ConstructBET(postfixExpressionStr);
+
+      buildString = "";  preorder(expressionTreeRootPtr);
+      cout << "Tree  pre-order expression is " << endl << buildString << endl << endl;
+
+      buildString = "";  inorder(expressionTreeRootPtr);
+      cout << "Tree   in-order expression is " << endl << buildString << endl << endl;
+
+      buildString = "";  postorder(expressionTreeRootPtr);
+      cout << "Tree post-order expression is " << endl << buildString << endl << endl;
+
+      cout << endl << endl;
+
+  };//while
+
+  inputFileStreamObj.close();
+
+  cout << "Press the enter key once or twice to end." << endl;  
+
+  return EXIT_SUCCESS;
+
+}//main()
+
